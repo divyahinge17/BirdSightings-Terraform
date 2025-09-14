93f9c940d09ea9296a707ca59172f6772728be7b
divyahinge17 <divya.hinge17@gmail.com>
2025-09-14 00:57:16 -0400

    Fixed Format

--- backend.tf
+++ backend.tf
@@ -1,16 +1,16 @@
-terraform{
-    backend "s3" {
-        bucket    = "div-tf-state"
-        region    = "us-east-1"
-        key       = "s3-github-actions/terraform.tfstate"
-        encrypt   = true
+terraform {
+  backend "s3" {
+    bucket  = "div-tf-state"
+    region  = "us-east-1"
+    key     = "s3-github-actions/terraform.tfstate"
+    encrypt = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
     }
-    required_version = ">=0.13.0"
-    required_providers {
-        aws = {
-            version = ">= 2.7.0"
-            source = "hashicorp/aws"
-        }
-    }
+  }
 }
 
