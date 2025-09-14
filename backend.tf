1eb2affadd07b0097668ec81ed151f5b495cc99e
divyahinge17 <divya.hinge17@gmail.com>
2025-09-13 23:20:30 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
     backend "s3" {
         bucket    = "terraform-state"
-        region    = "us-east-1"
+        region    = "us-east-2"
         key       = "s3-github-actions/terraform.tfstate"
         encrypt   = true
     }
